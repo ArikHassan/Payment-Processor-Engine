ecf40768212f1cfe41d85eb6d0c1086142fb935d
ArikHassan <arik-h@hotmail.com>
2026-04-19 09:08:00 -0400

    Added Merchant.java class

---
+++ src/main/java/org/example/Merchant.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Merchant {
+
+    // Map: Customer ID : Card Token
+    private Map<UUID, TokenizedCard> savedCards = new HashMap<>();
+
+    public void saveCard(UUID customerId, TokenizedCard cardToken) {
+        savedCards.put(customerId, cardToken);
+    }
+
+    public UUID getCardToken(UUID customerId) {
+        return savedCards.get(customerId).token;
+    }
+}
